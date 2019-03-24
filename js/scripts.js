61dc4e5db047c0efb047b5b42996a93ed0c63608
Jesse Wu <wuzhaoxing@hotmail.com>
2019-03-23 23:54:53 -0700

    Changes to buttons and animations

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-		$(".navigation").css("height", "70px");
-		$(".nav-phantom").css("height", "70px");
-		$(".nav-link").css("padding-top", "20px");
-		$(".nav-link").css("padding-bottom", "18px");
-		$(".nav-logo").css("width","32px");
-		$(".nav-logo").css("height","32px");
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		$(".navigation").css("height", "50px");
+		$(".nav-phantom").css("height", "50px");
+		$(".nav-link").css("padding-top", "10px");
+		$(".nav-link").css("padding-bottom", "8px");
+		$(".nav-logo").css("width","24px");
+		$(".nav-logo").css("height","24px");
 	}
 	else {
 		$(".navigation").css("height", "100px");
@@ -21,10 +21,10 @@ function scrollFunction() {
 	};
 
 	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-		// document.getElementById("navigation").style.height = "70px";
 		$(".navigation").css("background-color", "rgba(40,40,50,0.9)");
 	}
 	else {
 		$(".navigation").css("background-color", "rgba(40,40,50,1)");
 	};
 }
+
