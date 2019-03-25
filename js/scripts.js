0c32fed19508abd051586a93b68c23e8a30e2563
Jesse Wu <wuzhaoxing@hotmail.com>
2019-03-24 17:23:48 -0700

    Added back to top button to all project pages, added scrolling animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 /* My Scripts */
 
+
+
+
+// Smooth scrolling for anchor links
+
+$(document).ready(function(){
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+
+	    // Make sure this.hash has a value before overriding default behavior
+	    if (this.hash !== "") {
+	      // Prevent default anchor click behavior
+	      event.preventDefault();
+
+	      // Store hash
+	      var hash = this.hash;
+
+	      // Using jQuery's animate() method to add smooth page scroll
+	      // The optional number specifies the number of milliseconds it takes to scroll to the specified area
+	      $('html, body').animate({
+	      	scrollTop: $(hash).offset().top
+	      }, 350, function(){
+	        // Add hash (#) to URL when done scrolling (default click behavior)
+	        window.location.hash = hash;
+	    	});
+	    } // End if
+	});
+});
+
+
+
+
+
+// Minimize navbar and add back to top button, when not at top of page
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -26,5 +61,24 @@ function scrollFunction() {
 	else {
 		$(".navigation").css("background-color", "rgba(40,40,50,1)");
 	};
+
+
+	var backToTopButton = $("#back-to-top");
+	var showButtonThreshold = $("#table-of-contents-top").position().top + 100;
+	console.log(showButtonThreshold);
+
+	if (document.body.scrollTop > showButtonThreshold || document.documentElement.scrollTop > showButtonThreshold) {
+		backToTopButton.css("bottom", "20px");
+		backToTopButton.css("visibility", "visible");
+		backToTopButton.css("opacity", "1");
+	}
+	else {
+		backToTopButton.css("bottom", "0px");
+		backToTopButton.css("visibility", "hidden");
+		backToTopButton.css("opacity", "0");
+	};
 }
+
+
+
 
